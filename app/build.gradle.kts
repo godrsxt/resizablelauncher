491c8797b9c284042e5b34fa1462f364c9f3c503
godrsxt <rushabgshitre360@gmail.com>
2026-07-23 06:39:28 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application") version "8.7.0"
     id("org.jetbrains.kotlin.android") version "2.0.0"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0"
 }
 
 android {
